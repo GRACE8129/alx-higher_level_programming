b788e46c0e74a38182e98190a98987ea0eea347a
osimen grace <osimenobehi6@gmail.com>
2024-01-06 05:26:57 -0800

    replace an element

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """Replaces an element at a specific position in a list.
+    """
+
+    temp = my_list[:]
+    if idx < 0:
+        return temp
+    if idx > len(my_list) -1:
+        return temp
+    temp[idx] = element
+    return temp
