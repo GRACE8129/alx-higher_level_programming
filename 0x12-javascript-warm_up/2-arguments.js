aee72423e55dc3ee0858406d578a59e4334b48f2
osimen grace <osimenobehi6@gmail.com>
2024-04-11 19:36:23 +0100

    Array

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 const argcount = 'process.argv.length';
 
 if (argcount === 2) {     
-console.log('No argument');
+ console.log('No argument');
 } else if (argcount === 3) {
-console.log('Argument found');
+  console.log('Argument found');
 } else {
-console.log('Arguments found');
+  console.log('Arguments found');
 }
 
